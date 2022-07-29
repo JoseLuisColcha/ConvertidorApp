bcb13e8e3004dc2e627ae0a44eece85246afc4fe
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-07-29 18:29:07 -0500

    Agregar archivos de proyecto.

---
+++ ConvertidorApp/Views/ConvertPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConvertidorApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConvertPage : ContentPage
+    {
+        double cm;
+        double m;
+        public ConvertPage()
+        {
+            InitializeComponent();
+        }
+        private void Calculate()
+        {
+            cm = Convert.ToDouble(txtcm.Text);
+            m = cm / 100;
+            lblResultado.Text = m.ToString() + "m";
+
+        }
+        private void Validate()
+        {
+            if (!string.IsNullOrEmpty(txtcm.Text))
+            {
+                Calculate();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad", "OK");
+            }
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            Validate();
+        }
+
+        private void btnVolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ ConvertidorApp/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConvertidorApp.Views;
+
+namespace ConvertidorApp
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConvertPage());
+        }
+    }
+}
